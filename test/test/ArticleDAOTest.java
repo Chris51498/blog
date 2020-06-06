db40af95696d8a0407f02265194e94733f9b75ae
xujian-xj <2351704779@qq.com>
2020-06-06 18:36:00 +0800

    ArticleDAOTest测试类

---
+++ test/test/ArticleDAOTest.java
@@ -0,0 +1,50 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.blog.dao.ArticleDAO;
+
+class ArticleDAOTest {
+
+	ArticleDAO dao = new ArticleDAO();
+	
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByTrem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindRetArt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByID() {
+		fail("Not yet implemented");
+	}
+
+	
+	
+	@Test
+	void testFindAll() throws Exception {
+		System.out.println(dao.findAll());
+	}
+
+}
